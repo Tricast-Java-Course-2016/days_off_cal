d54d258915850286a4faece436be4271fca1c584
Tamas Balogh <tamas.balogh@lvs.co.uk>
2016-09-05 14:06:56 +0200

    6th commit.
    Adding of xml based configuration.

--- src/main/java/com/tricast/web/Main.java
+++ src/main/java/com/tricast/web/Main.java
@@ -3,7 +3,6 @@ package com.tricast.web;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
-import com.tricast.web.dao.WebServiceModule;
 
 public class Main extends GuiceServletContextListener {
 
--- src/main/java/com/tricast/web/WebServiceModule.java
+++ src/main/java/com/tricast/web/WebServiceModule.java
@@ -1,8 +1,9 @@
-package com.tricast.web.dao;
+package com.tricast.web;
 
 import static com.google.inject.matcher.Matchers.annotatedWith;
 import static com.google.inject.matcher.Matchers.any;
 
+import java.io.InputStream;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -22,6 +23,12 @@ import com.tricast.guice.JdbcTransactionInterceptor;
 import com.tricast.guice.ObjectMapperProvider;
 import com.tricast.guice.WorkspaceProvider;
 import com.tricast.web.annotations.JdbcTransaction;
+import com.tricast.web.dao.AccountDao;
+import com.tricast.web.dao.AccountDaoImpl;
+import com.tricast.web.dao.BlockedDaysDao;
+import com.tricast.web.dao.BlockedDaysDaoImpl;
+import com.tricast.web.dao.HolidayDao;
+import com.tricast.web.dao.HolidayDaoImpl;
 import com.tricast.web.manager.AccountManager;
 import com.tricast.web.manager.AccountManagerImpl;
 import com.tricast.web.manager.HolidayManager;
@@ -31,63 +38,75 @@ import com.tricast.web.server.HolidayService;
 
 public class WebServiceModule extends JerseyServletModule {
 
-	@Override
-	protected void configureServlets() {
-		final ResourceConfig rc = new PackagesResourceConfig("com.tricast.web.server");
+    @Override
+    protected void configureServlets() {
+        final ResourceConfig rc = new PackagesResourceConfig("com.tricast.web.server");
 
-		Names.bindProperties(binder(), loadProperties());
+        Names.bindProperties(binder(), loadProperties());
 
-		// Force the Injector to bind a Provider that provides DataSources
-		requireBinding(DataSource.class);
+        // Force the Injector to bind a Provider that provides DataSources
+        requireBinding(DataSource.class);
 
-		// Bind the DataSource class to be provided by OracleDataSourceProvider
-		// in Singleton
-		bind(DataSource.class).toProvider(PostGreSQLDataSourceProvider.class).in(Singleton.class);
-		bind(WorkspaceImpl.class).toProvider(WorkspaceProvider.class);
+        // Bind the DataSource class to be provided by OracleDataSourceProvider
+        // in Singleton
+        bind(DataSource.class).toProvider(PostGreSQLDataSourceProvider.class).in(Singleton.class);
+        bind(WorkspaceImpl.class).toProvider(WorkspaceProvider.class);
 
-		// Binding certain Interfaces to their Implementations. This can be
-		// easily modified for testing, to use Test Implementations
-		bind(AccountDao.class).to(AccountDaoImpl.class);
-		bind(AccountManager.class).to(AccountManagerImpl.class);
-		bind(HolidayDao.class).to(HolidayDaoImpl.class);
-		bind(HolidayManager.class).to(HolidayManagerImpl.class);
-		bind(BlockedDaysDao.class).to(BlockedDaysDaoImpl.class);
+        // Binding certain Interfaces to their Implementations. This can be
+        // easily modified for testing, to use Test Implementations
+        bind(AccountDao.class).to(AccountDaoImpl.class);
+        bind(AccountManager.class).to(AccountManagerImpl.class);
+        bind(HolidayDao.class).to(HolidayDaoImpl.class);
+        bind(HolidayManager.class).to(HolidayManagerImpl.class);
+        bind(BlockedDaysDao.class).to(BlockedDaysDaoImpl.class);
 
-		// Binding the REST endpoints
-		bind(AccountService.class);
-		bind(HolidayService.class);
+        // Binding the REST endpoints
+        bind(AccountService.class);
+        bind(HolidayService.class);
 
-		// Injections to use our own ObjectMappers. Only needed to pretty print
-		// the JSON output, not necessary at all.
-		bind(ObjectMapper.class).toProvider(ObjectMapperProvider.class).in(Singleton.class);
-		bind(JacksonJsonProvider.class).toProvider(JacksonJsonProviderProvider.class).in(Singleton.class);
+        // Injections to use our own ObjectMappers. Only needed to pretty print
+        // the JSON output, not necessary at all.
+        bind(ObjectMapper.class).toProvider(ObjectMapperProvider.class).in(Singleton.class);
+        bind(JacksonJsonProvider.class).toProvider(JacksonJsonProviderProvider.class).in(Singleton.class);
 
-		// Creating the JdbcUnitOfWorkInterceptor and also stating that it is
-		// required to bind this class.
-		JdbcTransactionInterceptor jdbcTransactionInterceptor = new JdbcTransactionInterceptor();
-		requestInjection(jdbcTransactionInterceptor);
+        // Creating the JdbcUnitOfWorkInterceptor and also stating that it is
+        // required to bind this class.
+        JdbcTransactionInterceptor jdbcTransactionInterceptor = new JdbcTransactionInterceptor();
+        requestInjection(jdbcTransactionInterceptor);
 
-		// Bind to intercept any calls annotated with JdbcUnitOfWork by
-		bindInterceptor(any(), annotatedWith(JdbcTransaction.class), jdbcTransactionInterceptor);
+        // Bind to intercept any calls annotated with JdbcUnitOfWork by
+        bindInterceptor(any(), annotatedWith(JdbcTransaction.class), jdbcTransactionInterceptor);
 
-		for (Class<?> resource : rc.getClasses()) {
-			System.out.println("Binding resource: " + resource.getName());
-			bind(resource);
-		}
+        for (Class<?> resource : rc.getClasses()) {
+            System.out.println("Binding resource: " + resource.getName());
+            bind(resource);
+        }
 
-		// Bind to serve all /services calls by this Servlet
-		serve("/services/*").with(GuiceContainer.class);
-	}
+        // Bind to serve all /services calls by this Servlet
+        serve("/services/*").with(GuiceContainer.class);
+    }
 
-	private Properties loadProperties() {
-		Properties props = new Properties();
-		props.put("datasourceName", "PostGreSQL Portable DB");
-		props.put("serverName", "localhost");
-		props.put("portNumber", "5432");
-		props.put("databaseName", "postgres");
-		props.put("user", "postgres");
-		props.put("password", "password");
-		return props;
-	}
+    private Properties loadProperties() {
+        InputStream is = null;
+        Properties props = new Properties();
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            is = classLoader.getResourceAsStream("setup.xml");
+            // Try loading properties from the file (if found)
+            props.loadFromXML(is);
+        } catch (Exception e) {
+            // Skip load use default
+        }
+        if (is == null) {
+            props.put("datasourceName", "PostGreSQL Portable DB");
+            props.put("serverName", "localhost");
+            props.put("portNumber", "5432");
+            props.put("databaseName", "postgres");
+            props.put("user", "postgres");
+            props.put("password", "password");
+        }
+
+        return props;
+    }
 
 }
